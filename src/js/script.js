49fb9fcd521f013570ebdac6f12eb10ffbec6546
Pruvost-Lison <lison.pruvost@edu.univ-fcomte.fr>
2024-12-03 15:24:50 +0100

    ajout de l'ouverture ouvrir de la lightbox et l'image

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,10 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImg = lightbox.querySelector('img');
 
 
+document.querySelectorAll("ul img").forEach(thumbnail => {
+    thumbnail.onclick = () => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.showModal();
+    };
+});
 
